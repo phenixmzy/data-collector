6568ee411d22bee27268a26fd9cf0d965e787a95
phenixmzy <wingmazhiyong@gmail.com>
2019-12-05 11:22:32 +0800

    update run_makedata_gameplay.sh

--- bin/run_makedata_gameplay.sh
+++ bin/run_makedata_gameplay.sh
@@ -6,4 +6,4 @@ for f in $BASE_DIR/lib/*.jar; do
         JARS=$JARS:$f
 done
 echo $JARS
-java -cp $JARS:$BASE_DIR/data-collector-${VERSION}.jar com.phenix.bigdata.test.Setup 50000 yzj-client-01:9092,yzj-client-02:9092,yzj-client-03:9092 gameplay-log-input 5 10000 100000000 30
+java -cp $JARS:$BASE_DIR/data-collector-${VERSION}.jar com.phenix.bigdata.test.Setup 50000 vmphenix:9092,vmnarsi:9092,vmramon:9092 gameplay-input 5 10000 100000000 30
