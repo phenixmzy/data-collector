b2cf582f2e33c87debc6e001906d1d64af528bf6
phenixmzy <wingmazhiyong@gmail.com>
2020-07-10 22:06:23 +0800

    update

--- src/main/java/com/phenix/bigdata/model/factory/GameBrowseFactory.java
+++ src/main/java/com/phenix/bigdata/model/factory/GameBrowseFactory.java
@@ -1,15 +1,15 @@
 package com.phenix.bigdata.model.factory;
 import com.phenix.bigdata.model.GameBrowse;
-import com.phenix.bigdata.model.GamePlay;
 
 public class GameBrowseFactory {
     private final static String IP_SPLIT = ".";
     private static final String[] GAME_TYPES = new String[]{"exe", "web", "onlie", "flash"};
     private static final String[] CHANNEL_FROMS = new String[]{"my", "category", "game_helper", "recommend", "726", "4399", "kuwo", "relateflash"};
     private static final String[] SITES = new String[]{"index", "kw", "qvod", "kugo", "qq", "qvod"};
+    private static final String[] CLIENT_VERSIONS = new String[] {"1.6.2","1.8.8","2.31.7","2.5.16","2.7.19","3.2.1","3.1.2","3.4.8"};
+    private static final String[] DRIVERS = new String[] {"PC","MAC PRO","MAC AIR","MI","HUAWEI MateBook","DELL", "LX", "SX", "HP"};
 
     public static GameBrowse build(int gameIdMaxNum, int userIdMaxNum, int maxDelay) {
-
         String gameId = String.valueOf((int)((Math.random()*9+1) * gameIdMaxNum));
         String userId = String.valueOf((long)((Math.random()*9+1) * userIdMaxNum));
         int currTimeStamp = (int)(System.currentTimeMillis()/1000) ;
@@ -18,7 +18,10 @@ public class GameBrowseFactory {
         String gameType = GAME_TYPES[getRandNum(0,4) % 4];
         String channelFrom = CHANNEL_FROMS[getRandNum(0,8) % 8];
         String site = SITES[getRandNum(0,6) % 6];
+        String clientVersion = CLIENT_VERSIONS[getRandNum(0,8) % 8];
+        String clientDriver = DRIVERS[getRandNum(0,9) % 9];
         String userIp = getUserIp();
+
         GameBrowse browse = new GameBrowse();
         browse.setGameId(gameId);
         browse.setUserId(userId);
@@ -28,6 +31,8 @@ public class GameBrowseFactory {
         browse.setSite(site);
         browse.setChannelFrom(channelFrom);
         browse.setUserIp(userIp);
+        browse.setClientVersion(clientVersion);
+        browse.setClientDriver(clientDriver);
 
         return browse;
     }
