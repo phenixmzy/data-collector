79307b3ca6cd63e45bb10701e6a6c0debd7d8b73
phenixmzy <wingmazhiyong@gmail.com>
2020-08-22 23:12:37 +0800

    update

---
+++ src/main/java/com/phenix/bigdata/model/GameInfo.java
@@ -0,0 +1,18 @@
+package com.phenix.bigdata.model;
+
+import java.util.List;
+
+public class GameInfo {
+    private int gameId;
+    private int gameName;
+    private int gameTypeId;
+    private long gameSize;
+    private int operatorType;
+    private String gameLabel;
+    private String developer;
+    private String language;
+    private int publishingPeriod;
+    private int updateTime;
+    private List<Integer> attrIds;
+
+}
---
+++ src/main/java/com/phenix/bigdata/model/UserInfo.java
@@ -0,0 +1,13 @@
+package com.phenix.bigdata.model;
+
+public class UserInfo {
+    private long userId;
+    private String sex;
+    private int age;
+    private String fromChannel;
+    private String geoCity;
+    private String profession;
+    private int education;
+    private String registerDriver;
+    private int constellatoryIdx;
+}
